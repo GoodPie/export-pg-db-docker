dc5b071a57ca8051c50985c5dbe6a36060a02bc3
Brandyn Britton <Brandynbb96@gmail.com>
2024-12-12 13:25:18 +0800

    update: added homembrew formula details

--- pg-docker-helper.rb
+++ pg-docker-helper.rb
@@ -4,13 +4,13 @@ class PgDockerHelper < Formula
   version "1.0.0"
 
   resource "import-pg-db-docker" do
-    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/1.0.0/import-pg-db-docker.sh"
-    sha256 ""
+    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/refs/tags/v1.0.0.0/import-pg-db-docker.sh"
+    sha256 "5f9803d538c82bdb69e8cab0b5eb0cf979f7499fe82b07b94eda28f197b98f3f"
   end
 
   resource "export-pg-db-docker" do
-    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/1.0.0/export-pg-db-docker.sh"
-    sha256 ""
+    url "https://raw.githubusercontent.com/GoodPie/export-pg-db-docker/3a0b96c6794fd20083b5a815cb8fbecacd9552a7/export-pg-db-docker.sh"
+    sha256 "e907933ac1ae57d1e588fa33f6c96f14a78491600d912ec54f6d5043386d38e2"
   end
 
   def install
